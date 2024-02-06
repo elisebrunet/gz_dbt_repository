6e09297926ebcce690bf5603c6e7643e096b0911
Elise <elisebrunet.lw@gmail.com>
2024-02-06 16:07:17 +0000

    modif orders margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,3 +1,10 @@
 SELECT 
-   *
+    date_date,
+    orders_id,
+    SUM(revenue) AS revenue,
+    SUM(quantity) AS quantity, 
+    SUM(purchase_cost) AS purchase_cost,
+    SUM(margin) AS margin, 
 FROM {{ ref('int_sales_margin') }}
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC, orders_id DESC
--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,11 +1,18 @@
 #Operational margin = margin + shipping fee - log_cost - ship_cost
 
 SELECT 
-    om.*,
-    sh.shipping_fee,
-    sh.log_cost,
-    sh.ship_cost,
-    om.margin + sh.shipping_fee - sh.log_cost - sh.ship_cost AS operational_margin
+    om.date_date,
+    om.orders_id,
+    SUM(om.revenue) AS revenue,
+    SUM(om.quantity) AS quantity, 
+    SUM(om.purchase_cost) AS purchase_cost,
+    SUM(om.margin) AS margin,
+    SUM(sh.shipping_fee) AS shipping_fee,
+    SUM(sh.log_cost) AS log_cost,
+    SUM(sh.ship_cost) AS ship_cost,
+    SUM(om.margin + sh.shipping_fee - sh.log_cost - sh.ship_cost) AS operational_margin
 FROM {{ ref('stg_raw__ship') }} as sh 
 LEFT JOIN {{ ref('int_orders_margin') }} AS om 
 USING (orders_id)
+GROUP BY orders_id, date_date
+ORDER BY date_date DESC, orders_id DESC
