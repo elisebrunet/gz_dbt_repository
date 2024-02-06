3a8d8e4b6e97e1bc3f9719a231fc3a99239bfa8e
Elise <elisebrunet.lw@gmail.com>
2024-02-06 13:57:36 +0000

    margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,13 @@
+WITH purchase_cost AS (
+    SELECT
+        s.revenue,
+        s.qty * (CAST(p.purchase_price AS FLOAT)) AS purchase_cost
+    FROM {{ ref('stg_raw__sales') }} AS s
+    INNER JOIN {{ ref('stg_raw__product') }} AS p 
+    ON s.products_id = p.products_id
+)
+
+SELECT 
+    revenue - purchase_cost AS margin
+FROM purchase_cost
+LIMIT 10
