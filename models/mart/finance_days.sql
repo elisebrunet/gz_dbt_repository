bb0a44ffd7834e3242bd8e88c2f92f98b53e009f
Elise <elisebrunet.lw@gmail.com>
2024-02-07 08:21:50 +0000

    finance_daily_updated

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -2,7 +2,7 @@ SELECT
     COUNT(orders_id) AS nbr_of_transactions,
     date_date,
     ROUND(SUM(revenue)) AS revenue,
-    ROUND(AVG(purchase_cost*quantity)) AS average_basket,
+    ROUND(AVG(revenue),1) AS average_basket,
     ROUND(SUM(margin)) AS daily_margin,
     ROUND(SUM(operational_margin)) AS daily_operational_margin,
 FROM {{ ref('int_orders_operational') }}
