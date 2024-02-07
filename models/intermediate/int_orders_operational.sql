97a99362085c96713320920792d7aeb75ab623c1
Elise <elisebrunet.lw@gmail.com>
2024-02-07 08:19:03 +0000

    finance_daily

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,16 +3,16 @@
 SELECT 
     om.date_date,
     om.orders_id,
-    SUM(om.revenue) AS revenue,
+    ROUND(SUM(om.revenue),2) AS revenue,
     SUM(om.quantity) AS quantity, 
-    SUM(om.purchase_cost) AS purchase_cost,
-    SUM(om.margin) AS margin,
-    SUM(sh.shipping_fee) AS shipping_fee,
-    SUM(sh.log_cost) AS log_cost,
-    SUM(sh.ship_cost) AS ship_cost,
-    SUM(om.margin + sh.shipping_fee - sh.log_cost - sh.ship_cost) AS operational_margin
+    ROUND(SUM(om.purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(om.margin),2) AS margin,
+    ROUND(SUM(sh.shipping_fee),2) AS shipping_fee,
+    ROUND(SUM(sh.log_cost),2) AS log_cost,
+    ROUND(SUM(sh.ship_cost),2) AS ship_cost,
+    ROUND(SUM(om.margin + sh.shipping_fee - sh.log_cost - sh.ship_cost),2) AS operational_margin
 FROM {{ ref('stg_raw__ship') }} as sh 
 LEFT JOIN {{ ref('int_orders_margin') }} AS om 
 USING (orders_id)
 GROUP BY orders_id, date_date
-ORDER BY date_date DESC, orders_id DESC
+ORDER BY date_date DESC, orders_id DESC, operational_margin DESC
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,4 +1,10 @@
 SELECT 
-    *
+    COUNT(orders_id) AS nbr_of_transactions,
+    date_date,
+    ROUND(SUM(revenue)) AS revenue,
+    ROUND(AVG(purchase_cost*quantity)) AS average_basket,
+    ROUND(SUM(margin)) AS daily_margin,
+    ROUND(SUM(operational_margin)) AS daily_operational_margin,
 FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
+ORDER BY date_date DESC
